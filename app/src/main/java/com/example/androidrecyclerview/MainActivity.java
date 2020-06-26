bb1a44e367f1346e4363be7bfc5f375017172fb9
Muhammad Hussain <ahsanpervez74@gmail.com>
2020-06-26 18:56:06 +0500

    Add New Activity to get input from user

--- app/src/main/java/com/example/androidrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/androidrecyclerview/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.androidrecyclerview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton addData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView programmingList = findViewById(R.id.programmingList);
         programmingList.setLayoutManager(new LinearLayoutManager(this));
-        String[] students = {"Hussain", "Azhar", "Ahsan", "Hamza", "Amir", "Haseeb", "Najeeb", "Hussain", "Azhar", "Ahsan", "Hamza", "Amir", "Haseeb", "Najeeb"};
+        String[] students = {"Hussain", "Azhar", "Ahsan"};
         programmingList.setAdapter(new ProgrammingAdapter(students));
+
+        addData = findViewById(R.id.addData);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddRecord.class));
+            }
+        });
     }
 }
